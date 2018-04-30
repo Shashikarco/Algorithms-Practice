3f7458f5c2a22d37f6772ec1f1b5bc8342a60a00
Shashikarco <35733999+Shashikarco@users.noreply.github.com>
2018-04-30 15:49:02 +0530

    commit

--- Practive.cpp
+++ Practive.cpp
@@ -1,7 +1,43 @@
-#include<iostream>
-
+// C++ program to rotate an array by 
+// d elements
+#include <bits/stdc++.h>
+using namespace std;
+ 
+/*Function to left Rotate arr[] of 
+  size n by 1*/
+void GetReversedarray(int arr[],int d,int n)
+{
+    for(int i = 0;i<d;i++)
+    {
+        int temp;
+        int j=0;
+        temp = arr[i];
+        for(j = 0 ; j<n-1;j++)
+        {
+           
+            arr[j] = arr[j+1];          
+        }
+        arr[j] = temp;
+    }
+}
+/* utility function to print an array */
+void printArray(int arr[], int size)
+{
+    for(int i = 0; i < size; i++)
+    cout << arr[i] << " ";
+}
+ 
+/* Driver program to test above functions */
 int main()
 {
-    std::cout<<"hello";
-    std::cin.get();
+   int arr[] = {1, 2, 3, 4, 5, 6, 7};
+   int n = sizeof(arr) / sizeof(arr[0]);
+    
+   // Function calling
+   GetReversedarray(arr,2,n);
+   //leftRotate(arr, 2, n);
+   printArray(arr, n);
+   std::cin.get();
+  
+   return 0;
 }
